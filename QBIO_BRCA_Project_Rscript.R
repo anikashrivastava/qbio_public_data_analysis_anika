ca46e53fff961804f76d8c8b8ec45eccf7420153
anikashr <anikashr@discovery1.usc.edu>
2021-03-25 16:00:09 -0700

    uncommented figures

--- QBIO_BRCA_Project_Rscript.R
+++ QBIO_BRCA_Project_Rscript.R
@@ -66,51 +66,51 @@ old_maf <- subsetMaf(maf_dataframe, tsb = old_codes$Tumor_Sample_Barcode)
 
 
 #get oncoplots
-#oncoplot(maf = maf_dataframe, draw_titv = TRUE, top = 3)
-#oncoplot(maf = young_maf, draw_titv = TRUE, top = 3)
-#oncoplot(maf = mid_maf, draw_titv = TRUE, top = 3)
-#oncoplot(maf = old_maf, draw_titv = TRUE, top = 3)
+oncoplot(maf = maf_dataframe, draw_titv = TRUE, top = 3)
+oncoplot(maf = young_maf, draw_titv = TRUE, top = 3)
+oncoplot(maf = mid_maf, draw_titv = TRUE, top = 3)
+oncoplot(maf = old_maf, draw_titv = TRUE, top = 3)
 #get boxplots for top 3 genes for each age group
 
-# htseq_counts <- assays(sum_exp)$"HTSeq - Counts"
+ htseq_counts <- assays(sum_exp)$"HTSeq - Counts"
 # 
 # #Genes of interest: TP53, PIK3CA, TTN, GATA3
 # #external_gene_name does NOT need quotation marks because there are no spaces in the name of the column
-# TP53_mask <- rowData(sum_exp)$external_gene_name == "TP53"
-# TP53_ENSG_name <- rowData(sum_exp)$ensembl_gene_id[TP53_mask]
+TP53_mask <- rowData(sum_exp)$external_gene_name == "TP53"
+ TP53_ENSG_name <- rowData(sum_exp)$ensembl_gene_id[TP53_mask]
 # 
-# PIK3CA_mask <- rowData(sum_exp)$external_gene_name == "PIK3CA"
-# PIK3CA_ENSG_name <- rowData(sum_exp)$ensembl_gene_id[PIK3CA_mask]
+PIK3CA_mask <- rowData(sum_exp)$external_gene_name == "PIK3CA"
+PIK3CA_ENSG_name <- rowData(sum_exp)$ensembl_gene_id[PIK3CA_mask]
 # 
-# TTN_mask <- rowData(sum_exp)$external_gene_name == "TTN"
-# TTN_ENSG_name <- rowData(sum_exp)$ensembl_gene_id[TTN_mask]
+TTN_mask <- rowData(sum_exp)$external_gene_name == "TTN"
+TTN_ENSG_name <- rowData(sum_exp)$ensembl_gene_id[TTN_mask]
 # 
-# GATA3_mask <- rowData(sum_exp)$external_gene_name == "GATA3"
-# GATA3_ENSG_name <- rowData(sum_exp)$ensembl_gene_id[GATA3_mask]
+GATA3_mask <- rowData(sum_exp)$external_gene_name == "GATA3"
+GATA3_ENSG_name <- rowData(sum_exp)$ensembl_gene_id[GATA3_mask]
 # 
 # #get HTseq counts for desired genes
-# patient_data$TP53_counts_log = sapply(htseq_counts[TP53_ENSG_name,], log10)
-# patient_data$PIK3CA_counts_log = sapply(htseq_counts[PIK3CA_ENSG_name,], log10)
-# patient_data$TTN_counts_log = sapply(htseq_counts[TTN_ENSG_name,], log10)
-# patient_data$GATA3_counts_log = sapply(htseq_counts[GATA3_ENSG_name,], log10)
+patient_data$TP53_counts_log = sapply(htseq_counts[TP53_ENSG_name,], log10)
+patient_data$PIK3CA_counts_log = sapply(htseq_counts[PIK3CA_ENSG_name,], log10)
+patient_data$TTN_counts_log = sapply(htseq_counts[TTN_ENSG_name,], log10)
+patient_data$GATA3_counts_log = sapply(htseq_counts[GATA3_ENSG_name,], log10)
 # 
 # 
 # # Boxplots by age
-# jpeg("TP53_log_counts_by_age.jpg")
-# boxplot(TP53_counts_log~age_category, data = patient_data, main = "Boxplot of HTSeq - Counts for TP53 by Age Category")
-# dev.off()
+jpeg("TP53_log_counts_by_age.jpg")
+boxplot(TP53_counts_log~age_category, data = patient_data, main = "Boxplot of HTSeq - Counts for TP53 by Age Category")
+dev.off()
 # 
-# jpeg("PIK3CA_log_counts_by_age.jpg")
-# boxplot(PIK3CA_counts_log~age_category, data = patient_data, main = "Boxplot of HTSeq - Counts for PIK3CA by Age Category")
-# dev.off()
+jpeg("PIK3CA_log_counts_by_age.jpg")
+boxplot(PIK3CA_counts_log~age_category, data = patient_data, main = "Boxplot of HTSeq - Counts for PIK3CA by Age Category")
+dev.off()
 # 
-# jpeg("TTN_log_counts_by_age.jpg")
-# boxplot(TTN_counts_log~age_category, data = patient_data, main = "Boxplot of HTSeq - Counts for TTN by Age Category")
-# dev.off()
+jpeg("TTN_log_counts_by_age.jpg")
+boxplot(TTN_counts_log~age_category, data = patient_data, main = "Boxplot of HTSeq - Counts for TTN by Age Category")
+dev.off()
 # 
-# jpeg("GATA3_log_counts_by_age.jpg")
-# boxplot(GATA3_counts_log~age_category, data = patient_data, main = "Boxplot of HTSeq - Counts for GATA3 by Age Category")
-# dev.off()
+jpeg("GATA3_log_counts_by_age.jpg")
+boxplot(GATA3_counts_log~age_category, data = patient_data, main = "Boxplot of HTSeq - Counts for GATA3 by Age Category")
+dev.off()
 # 
 clin_query <- GDCquery(project = "TCGA-BRCA", data.category="Clinical", file.type = "xml")
 GDCdownload( clin_query ) #only need this command once. This downloads the files onto your system.
